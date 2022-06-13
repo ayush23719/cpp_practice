5db1fe11358eec312f9434b870259123dd22665b
Ayush Pratap Singh <91633771+ayush23719@users.noreply.github.com>
2022-06-14 00:05:48 +0530

    stl practice

--- set_stl.cpp
+++ set_stl.cpp
@@ -50,4 +50,6 @@ int main()
     cout << "Find the element 8 in the set: " << *itr << endl;
 
     // print elements after iterator(8) in the set
+    for (auto it = itr; it != s.end(); it++)
+        cout << *it << " ";
 }
